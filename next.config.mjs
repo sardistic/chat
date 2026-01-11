c855408fbdf1f38c1457eb3e6006cfb3d30132a2
sardistic <coldhunter@gmail.com>
2026-01-11 01:55:33 -0500

    Config: Added Permissions-Policy headers to allow camera and microphone access.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=*, microphone=*, geolocation=()'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
