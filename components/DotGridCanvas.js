839636ad63ccd844a9c86459f8abed8421664fd6
sardistic <coldhunter@gmail.com>
2026-01-19 05:03:43 -0500

    Ultra-fast return to origin (0.85 ease)

--- components/DotGridCanvas.js
+++ components/DotGridCanvas.js
@@ -33,7 +33,7 @@ export default function DotGridCanvas({ className = '', zoomLevel = 0 }) {
             waveSpeed: 0.002,
             growth: 40, // Mouse proximity growth
             proximity: 350, // Mouse proximity radius
-            ease: 0.4, // Much faster return to origin
+            ease: 0.85, // Near instant return
             particleCount: 15,
             particleSpeed: 0.2,
             particleRadius: 1,
